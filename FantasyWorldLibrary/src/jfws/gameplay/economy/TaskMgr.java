bb2817aa8c9ef1bcab652765cacc5c245a987477
Orchaldir <orchaldir@gmail.com>
2015-07-05 16:21:24 +0200

    Added class TaskMgr.

---
+++ FantasyWorldLibrary/src/jfws/gameplay/economy/TaskMgr.java
@@ -0,0 +1,37 @@
+package jfws.gameplay.economy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskMgr
+{
+	private Map<String,Task> tasks_ = new HashMap<>();
+	
+	// Task
+	
+	public Task createTask(String name)
+	{
+		if(name == null)
+			throw new IllegalArgumentException("Name is null!");
+		else if(name.isEmpty())
+			throw new IllegalArgumentException("Name is empty!");
+		else if(tasks_.containsKey(name))
+			throw new IllegalArgumentException("Task \"" + name + "\" already exists!");
+		
+		Task task = new Task(name);
+		
+		tasks_.put(name, task);
+		
+		return task;
+	}
+	
+	public Task getTask(String name)
+	{
+		if(name == null)
+			throw new IllegalArgumentException("Name is null!");
+		else if(name.isEmpty())
+			throw new IllegalArgumentException("Name is empty!");
+		
+		return tasks_.get(name);
+	}
+}
---
+++ FantasyWorldLibrary/test/jfws/gameplay/economy/TaskMgrTest.java
@@ -0,0 +1,67 @@
+package jfws.gameplay.economy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class TaskMgrTest
+{
+	static private String name_ = "Task0";
+	static private TaskMgr task_mgr_;
+	
+	@Before
+	public void setUp()
+	{
+		task_mgr_ = new TaskMgr();
+	}
+	
+	// Task
+	
+	@Test
+	public void testCreateTask()
+	{
+		Task task = task_mgr_.createTask(name_);
+		
+		assertNotNull(task);
+		assertEquals(name_, task.getName());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateTaskWithNull()
+	{
+		task_mgr_.createTask(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateTaskWithEmptyString()
+	{
+		task_mgr_.createTask("");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateTaskTwice()
+	{
+		task_mgr_.createTask(name_);
+		task_mgr_.createTask(name_);
+	}
+	
+	@Test
+	public void testGetTask()
+	{
+		Task task = task_mgr_.createTask(name_);
+		
+		assertEquals(task, task_mgr_.getTask(name_));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetTaskWithNull()
+	{
+		task_mgr_.getTask(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetTaskWithEmptyString()
+	{
+		task_mgr_.getTask("");
+	}
+}
